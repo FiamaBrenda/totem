aa0268cb3f6a20be732080f39efb2603ed13bf3d
Fiama Brenda <fiamabrenda94@gmail.com>
2025-11-26 14:54:11 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/",                           // ✅ IMPORTANTE PARA FUNCIONAR NO VERCEL
   server: {
     host: "::",
     port: 8080,
